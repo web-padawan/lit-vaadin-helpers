7b55a81b1ce72e9b6c52442ddb0cad12fa7208ac
web-padawan <iamkulykov@gmail.com>
2020-10-21 16:16:21 +0300

    Update grid demo: inline renderers

--- demo/grid-renderer-directive-demo.ts
+++ demo/grid-renderer-directive-demo.ts
@@ -4,15 +4,7 @@ import '@vaadin/vaadin-grid/vaadin-grid-column-group';
 import '@vaadin/vaadin-text-field';
 import type { TextFieldElement } from '@vaadin/vaadin-text-field';
 import type { User, HasFilter } from './types';
-import { gridRenderer, GridLitRenderer } from '../src/grid-renderer-directive';
-
-const indexRenderer: GridLitRenderer<User, HasFilter> = (model) => html`${model.index}`;
-
-const firstNameRenderer: GridLitRenderer<User, HasFilter> = (model, { filter }) => {
-  const name = model.item.name.first;
-  const match = filter && name.indexOf(filter) > -1;
-  return match ? html`<b>${name}</b>` : html`${name}`;
-};
+import { gridRenderer, GridModel } from '../src/grid-renderer-directive';
 
 class GridRendererDirectiveDemo extends LitElement implements HasFilter {
   @property({ attribute: false }) users!: User[];
@@ -34,13 +26,20 @@ class GridRendererDirectiveDemo extends LitElement implements HasFilter {
           flex-grow="0"
           width="60px"
           text-align="end"
-          .renderer="${gridRenderer(indexRenderer)}"
+          .renderer="${gridRenderer((model: GridModel<User>) => html`${model.index}`)}"
         ></vaadin-grid-column>
         <vaadin-grid-column-group header="Name">
           <vaadin-grid-column
             header="First"
             width="calc(20% - 12px)"
-            .renderer="${gridRenderer(firstNameRenderer, [this.filter])}"
+            .renderer="${gridRenderer(
+              (model: GridModel<User>, { filter }: HasFilter) => {
+                const name = model.item.name.first;
+                const match = filter && name.indexOf(filter) > -1;
+                return match ? html`<b>${name}</b>` : html`${name}`;
+              },
+              [this.filter]
+            )}"
           ></vaadin-grid-column>
           <vaadin-grid-column path="name.last" width="calc(20% - 12px)"></vaadin-grid-column>
         </vaadin-grid-column-group>
